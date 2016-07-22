6d05258ed9557dc36e74f314a1c02fc183a2201b
ankita <ankita.patil2@tcs.com>
2016-07-22 14:13:09 +0530

    Commit addjava file

--- Ewallet_devops/src/controller/Add.java
+++ Ewallet_devops/src/controller/Add.java
@@ -8,11 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import service.BenService;
 import bean.BenBean;
 
-
-//sample commit
-
-
-
 /**
  * Servlet implementation class Add
  */
@@ -30,6 +25,7 @@ public class Add extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		}
@@ -38,41 +34,23 @@ public class Add extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 	
-
 		BenBean bean = new BenBean();
-
 		long mob = Long.parseLong(request.getParameter("mobile"));
-
 		bean.setMobile(mob);
-	//	System.out.println(mob);
 		String name = request.getParameter("name");
-	//	System.out.println(name);
 		bean.setName(name);
-
 		String email = request.getParameter("email");
-		//System.out.println(email);
 		bean.setEmail(email);
-
-		// request.setAttribute("mobile", mob);
-
 		 request.setAttribute("name", name);
 		 request.setAttribute("email", email);
-		
-		 
-		 /* RequestDispatcher reqDispatcher = getServletConfig().getServletContext().getRequestDispatcher("javaPapers.jsp");
-	        reqDispatcher.forward(request,response);*/
-		/* request.getRequestDispatcher("javaPapers.jsp").forward(request,response);*/
+		 BenService bs= new BenService();
+			bs.addBeneficiary(bean);
 		 RequestDispatcher rd=request.getRequestDispatcher("AddStatus.jsp");
 	        rd.forward(request,response);
-		 
-		 BenService bs= new BenService();
-		
-			bs.addBeneficiary(bean);
-		
-		
 	}
 
 }
