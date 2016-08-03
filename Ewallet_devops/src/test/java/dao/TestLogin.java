0aff03de339e284053eb572a86ba968eb099ffa5
962572 <962572@01HW614632.India.TCS.com>
2016-08-03 11:50:08 +0530

    login test

--- Ewallet_devops/src/test/java/dao/TestLogin.java
+++ Ewallet_devops/src/test/java/dao/TestLogin.java
@@ -1,5 +1,30 @@
 package dao;
 
+import bean.User;
+import dao.LoginDao;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TestLogin {
+
+	public void loginSuccess() {
+		LoginDao user = new LoginDao();
+		User user1 = new User();
+		long num = 9096438884l;
+		user1 = user.validate(num, "password");
+		assertEquals("login successful", "anks", user1.getFirst());
+		
+	}
+		
+		public void loginFailure() {
+			LoginDao user = new LoginDao();
+			User user1 = new User();
+			long num = 9096438885l;
+			user1 = user.validate(num, "password");
+			assertEquals("login failure", "anks", user1.getFirst());
+
+
+	}
 
 }
