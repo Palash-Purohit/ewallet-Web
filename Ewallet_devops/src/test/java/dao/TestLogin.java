57048cc10319f1902853e63d4b6c386a4e690ce5
ankita <ankita.patil2@tcs.com>
2016-08-04 11:12:21 +0530

    gtfch

--- Ewallet_devops/src/test/java/dao/TestLogin.java
+++ Ewallet_devops/src/test/java/dao/TestLogin.java
@@ -17,5 +17,15 @@ public class TestLogin{
 		assertEquals("login successful", "ankita", user1.getFirst());
 		
 	}
+@Test
+public void loginFail() {
+	LoginDao user = new LoginDao();
+	User user1 = new User();
+	long num = 9093638888l;
+	user1 = user.validate(num, "password");
+	System.out.println("---------------test running-----------------------------------------");
 
+	assertEquals("login successful", null, user1.getFirst());
+	
+}
 }
